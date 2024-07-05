0ffb6e1e962b30663c5c320d4ae2ee01258b667c
venkatesh7975 <venkateh7975@gmail.com>
2024-07-05 20:01:53 +0530

    initial repo

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 
-import React from "react";
-
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
